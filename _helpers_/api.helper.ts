6dba3f8aa9595854dd2a28d6ee2caa0ea02b05d8
Martin <martin@Immas-MacBook-Pro.local>
2023-05-05 10:25:25 +0700

    update post_campaign.spec

--- _helpers_/api.helper.ts
+++ _helpers_/api.helper.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { base_url, token, randomName, randomSlug } from '../_helpers_/const'
+import { base_url, token, randomName, randomSlug, randomDescription } from '../_helpers_/const'
 
 export function createCampaign(name: string, slug: string, pub: boolean) {
     return request(base_url)
@@ -20,5 +20,28 @@ export async function deleteCampaign(id: number) {
         console.log('successfull delete id ' + id);
     } else {
         console.log('failed delete');
+    }
+}
+
+export function createChannel(name: string, des: string, color: string, star: boolean) {
+    return request(base_url)
+        .post("/api/channel/add")
+        .set('Authorization', token)
+        .send({
+            "name": name,
+            "description": des,
+            "color": color,
+            "starred": star
+        });
+}
+
+export async function deleteChannel(id: number) {
+    const response = await request(base_url)
+        .delete("/api/channel/" + id + "/delete")
+        .set('Authorization', token);
+    if (response.body.error === 0) {
+        console.log('successfull delete campaign id ' + id);
+    } else {
+        console.log('failed delete campaign');
     }
 }
--- _tests_/POST_campaign.spec.ts
+++ _tests_/POST_campaign.spec.ts
@@ -5,41 +5,47 @@ import { createCampaign, deleteCampaign } from '../_helpers_/api.helper'
 describe('POST /api/campaign/add', function () {
     test('CAM-001 Verify that user can create a campaign', async function () {
         const response = await createCampaign(randomName, randomSlug, true);
+        try {
+            expect(response.status).toEqual(200);
+            expect(response.body.error).toEqual(0);
+            expect(response.body.campaign.toLowerCase()).toEqual(randomName);
+            expect(response.body.public).toEqual(true);
+            expect(response.body.rotator).toEqual("https://urlbae.com/r/" + randomSlug);
+            expect(response.body).toHaveProperty("error");
+            expect(response.body).toHaveProperty("id");
+            expect(response.body).toHaveProperty("campaign");
+            expect(response.body).toHaveProperty("public");
+            expect(response.body).toHaveProperty("rotator");
+            expect(response.body).toHaveProperty("list");
+        } catch (error) {
 
-        expect(response.status).toEqual(200);
-        expect(response.body.error).toEqual(0);
-        expect(response.body.campaign.toLowerCase()).toEqual(randomName);
-        expect(response.body.public).toEqual(true);
-        expect(response.body.rotator).toEqual("https://urlbae.com/r/" + randomSlug);
-        expect(response.body).toHaveProperty("error");
-        expect(response.body).toHaveProperty("id");
-        expect(response.body).toHaveProperty("campaign");
-        expect(response.body).toHaveProperty("public");
-        expect(response.body).toHaveProperty("rotator");
-        expect(response.body).toHaveProperty("list");
-
-        // delete campaign after test
-        deleteCampaign(response.body.id);
+        } finally {
+            // delete campaign after test
+            await deleteCampaign(response.body.id);
+        }
     });
 
     test('CAM-011 Verify that user cannot create a campaign with existent "name" in Body', async function () {
         // get existent name of created campaign
         const response_1 = await createCampaign(randomName, randomSlug, true);
+        console.log(response_1.body);
         const existent_name = response_1.body.campaign;
         console.log('existent_name: ' + existent_name);
         // create the second campaign with existent name
         const response_2 = await createCampaign(existent_name, randomSlug, true);
+        try {
+            expect(response_2.status).toEqual(400);
+            expect(response_2.body.error).toEqual(1);
+            expect(response_2.body.message).toEqual("You already have a campaign with that name.");
+        } catch (error) {
 
-        expect(response_2.status).toEqual(200);
-        expect(response_2.body.error).toEqual(1);
-        expect(response_2.body.message).toEqual("You already have a campaign with that name.");
-
-        // delete the first campaign
-        deleteCampaign(response_1.body.id);
-
-        // delete the second campaign if it's created
-        if (response_2.body.error === 0) {
-            deleteCampaign(response_2.body.id);
+        } finally {
+            // delete the first campaign
+            await deleteCampaign(response_1.body.id);
+            // delete the second campaign if it's created
+            if (response_2.body.error === 0) {
+                await deleteCampaign(response_2.body.id);
+            }
         }
     });
 });
